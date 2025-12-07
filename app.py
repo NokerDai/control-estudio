7cee1b619452701932d71648d70d5f6780861a5d
NokerDai <149909989+NokerDai@users.noreply.github.com>
2025-12-07 17:58:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -510,12 +510,11 @@ st.markdown(f"""
         <div style="width:100%; background-color:#333; border-radius:10px; height:12px; margin: 15px 0;">
             <div style="width:{progreso_pct}%; background-color:{color_bar}; height:100%; border-radius:10px; transition: width 0.5s;"></div>
         </div>
-        <div style="display:flex; justify-content:space-between; color:#888;">
-            <div>{objetivo_hms} | ${pago_objetivo:.2f}</div>
-            <div style="margin-top:6px; font-size:0.95rem; color:#bbb;">
-                Semana: <span style="color:{semana_color}; font-weight:bold;">{semana_str}</span>
+            <div style="display:flex; justify-content:space-between; color:#888;">
+                <div style="margin-top:6px; font-size:0.95rem; color:#bbb;">
+                    <span style="color:{semana_color}; font-weight:bold;">{semana_str}</span> | {objetivo_hms} | ${pago_objetivo:.2f}
+                </div>
             </div>
-        </div>
     </div>
 """, unsafe_allow_html=True)
 
@@ -672,6 +671,7 @@ for materia, info in mis_materias.items():
                 st.rerun()
             except Exception as e:
                 st.error("Formato inválido")
+
 
 
 
