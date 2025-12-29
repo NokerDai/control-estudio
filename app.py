52f6ea8c42b41776b67e7d9eb1a6c2108c2898aa
Facundo <facutoitaly@gmail.com>
2025-12-29 10:26:47 -0300

    a

--- app.py
+++ app.py
@@ -166,7 +166,7 @@ if "password" in query_params and not st.session_state.authenticated:
         if st.session_state.usuario_seleccionado != "Facundo":
             handle_user_login("Facundo")
     
-if st.session_state.usuario_seleccionado is None:
+if "ivan" in query_params and st.session_state.usuario_seleccionado is None:
     handle_user_login("Iván")
 
 # ---------------------------------------------------------
