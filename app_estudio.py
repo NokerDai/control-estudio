27d5a9fa561197b699479ccf4ea55160556907a3
NokerDai <149909989+NokerDai@users.noreply.github.com>
2026-06-30 21:30:53 -0300

    Update HTML output format for end time display

--- app_estudio.py
+++ app_estudio.py
@@ -637,9 +637,9 @@ def main():
         if minutos_restantes > 0:
             # Calculamos a qué hora termina sumando los minutos que faltan a la hora actual
             hora_fin_obj = _argentina_now_global() + timedelta(minutes=minutos_restantes)
-            hora_fin_html = f'<div style="color:#00e676;">Terminás a las {hora_fin_obj.strftime("%H:%M")}</div>'
+            hora_fin_html = f'<div style="color:#888;">-> {hora_fin_obj.strftime("%H:%M")}</div>'
         else:
-             = '<div></div>'
+            hora_fin_html = f'<div></div>'
 
     if mostrar_dinero_detallado:
         # Caso Facundo: Muestra dinero en todos lados
